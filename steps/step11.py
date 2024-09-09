445f6163036a625f92b9d3d5438daf2dbc36aa66
ShntrIto <ito.shintaro.r2@dc.tohoku.ac.jp>
2024-09-09 10:02:42 +0900

    Use rest parameters

---
+++ steps/step11.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supportted'.format(type(data)))
+        self.data = data
+        self.grad = None
+        self.creator = None # 変数にとって生みの親である関数
+    
+    def set_creator(self, func):
+        '''
+        creatorを指定しない変数も存在するので，set_creatorという関数が必要
+        '''
+        self.creator = func
+    
+    def backward(self):
+        if self.grad is None: # grad の初期化に使用する
+            self.grad = np.ones_like(self.data) # self.data と型も同じになるs
+
+        funcs = [self.creator] # 次の関数を見つけるたびにここに追加される
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad) # backward メソッドを呼び出す
+
+            if x.creator is not None:
+                funcs.append(x.creator) # ひとつ前の関数をリストに追加
+
+class Function:
+    def __call__(self, inputs):
+        '''
+        入力が複数ある場合に対応させる
+        '''
+        xs = [x.data for x in inputs]
+        ys = self.forward(xs)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs
+    
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+
+# 手動でバックプロパゲーションを実装
+class Square(Function):
+    def forward(self, x):
+        y = x **2
+        return y
+    
+    def backward(self, gy):
+        '''
+        gy: 出力側から伝わる微分値
+        '''
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+    
+class Add(Function):
+    def forward(self, xs):
+        x0, x1 = xs
+        y = x0 + x1
+        return (y,) # タプルを返す
+
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+xs = [Variable(np.array(2)), Variable(np.array(3))] # リストとして変数を準備
+f = Add()
+ys = f(xs) # ysはタプルとして返される（それで問題ない？）
+y = ys[0]
+print(y.data)
